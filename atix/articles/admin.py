c0cff5007f53e25f3fe874a4ba753570d82ed2c9
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2008-11-30 15:21:27 -0400

    updated administration interface.

--- atix/articles/admin.py
+++ atix/articles/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from models import Article, Category
 
-admin.site.register(Article)
-admin.site.register(Category)
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal = ('categories',)
+    list_display = ('title', 'published', 'last_updated')
+    list_filter = ('categories',)
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'intro', 'body')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
