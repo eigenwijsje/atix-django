6508df78921eabcef76cb877b0a9600858c7f0e6
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2008-11-30 15:08:46 -0400

    corrected administration interface to match article on issue 5 of ATIX magazine.

--- atix/articles/admin.py
+++ atix/articles/admin.py
@@ -2,16 +2,5 @@ from django.contrib import admin
 
 from models import Article, Category
 
-class ArticleAdmin(admin.ModelAdmin):
-    filter_horizontal = ('categories',)
-    list_display = ('title', 'published', 'last_updated')
-    list_filter = ('categories',)
-    prepopulated_fields = {'slug': ('title',)}
-    search_fields = ('title', 'intro', 'body')
-
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug',)
-    prepopulated_fields = {'slug': ('name',)}
-
-admin.site.register(Article, ArticleAdmin)
-admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article)
+admin.site.register(Category)
