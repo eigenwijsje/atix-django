b221be520189dceee8e8ed1613096cb0d8144236
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2008-11-30 15:22:38 -0400

    added article detail view.

--- atix/articles/views.py
+++ atix/articles/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import get_object_or_404, render_to_response
+
+from models import Article
+
+def article_detail(request, year, month, slug):
+    article = get_object_or_404(Article,
+        published__year=year,
+        published__month=month,
+        slug=slug)
+
+    return render_to_response('articles/article_detail.html',
+        {'article': article})
